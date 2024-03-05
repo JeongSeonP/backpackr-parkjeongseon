0913a5b360b7199eae406fc54d8b4e0a17fe5a42
JeongSeonP <finchblue@naver.com>
2024-03-06 00:38:10 +0900

    :tada: 기본 세팅 완료
    프로젝트 기본 설정 및 Profile 컴포넌트 추가함

---
+++ components/profile/profile.tsx
@@ -0,0 +1,34 @@
+import styles from "./profile.module.sass";
+import { MdEmail } from "react-icons/md";
+import { PiPhoneFill, PiUserFill } from "react-icons/pi";
+
+function Profile() {
+  const profileData = {
+    name: "박정선",
+    phone: "010-9792-9767",
+    email: "finchblue8@gmail.com",
+  } as const;
+
+  const profileIcon = {
+    name: <PiUserFill size={16} />,
+    phone: <PiPhoneFill size={16} />,
+    email: <MdEmail size={16} />,
+  } as const;
+
+  type ProfileIconType = typeof profileData;
+
+  return (
+    <ul className={styles.profileList}>
+      {Object.entries(profileData).map(([key, value]) => (
+        <li key={key} className={styles.profileItem}>
+          <div className={styles.profileIcon}>
+            {profileIcon[key as keyof ProfileIconType]}
+          </div>
+          <div>{value}</div>
+        </li>
+      ))}
+    </ul>
+  );
+}
+
+export default Profile;
