aae7b5dc0027583f2d76b16b8ae35a14811dfd56
JeongSeonP <finchblue@naver.com>
2024-03-07 02:02:30 +0900

    :lipstick: Stylesheet 업데이트
    _mixins추가 및 _variables 업데이트하고 page & section & heading 마이너한 css조정함

--- next.config.js
+++ next.config.js
@@ -4,6 +4,9 @@ const path = require("path");
 module.exports = {
   sassOptions: {
     includePaths: [path.join(__dirname, "styles")],
-    prependData: `@import "styles/_variables.sass"`,
+    prependData: `
+    @import "styles/_variables.sass"
+    @import "styles/_mixins.sass"
+    `,
   },
 };
