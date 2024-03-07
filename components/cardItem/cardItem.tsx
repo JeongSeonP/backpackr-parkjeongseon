1fa03970d79b7b6de683b91d6d79ba1462efcfe0
JeongSeonP <finchblue@naver.com>
2024-03-07 19:00:29 +0900

    :adhesive_bandage: CardItem에 링크 태그 추가
    link가 필요할 것으로 예상되어 추가함

--- components/cardItem/cardItem.tsx
+++ components/cardItem/cardItem.tsx
@@ -7,6 +7,7 @@ import {
 import Image from "next/image";
 import defaultThumbnail from "@/public/assets/thumbnail.jpg";
 import Rating from "../rating/rating";
+import Link from "next/link";
 
 interface CardItemProps {
   direction: CardDirections;
@@ -21,66 +22,76 @@ export default function CardItem({ direction, data }: CardItemProps) {
     const verticalData = data as VerticalCardData;
     return (
       <li className={`${styles.cardItem} ${cardStyle}`}>
-        <div className={styles.cardThumbCover}>
-          <Image
-            src={verticalData.thumbnail ?? defaultThumbnail}
-            sizes="33vw"
-            fill
-            alt="썸네일 이미지"
-            className={styles.cardThumbImage}
-          />
-        </div>
-        <div>
-          <div className={styles.cardTop}>
-            <div className={styles.cardLabel}>{verticalData.cardLabel}</div>
-            <div className={styles.cardTitle}>{verticalData.cardTitle}</div>
-            <div className={styles.cardText}>
-              <div className={styles.highlightText}>
-                {verticalData.highlightText}
-              </div>
-              <div className={styles.crossOutText}>
-                {verticalData.crossOutText}
-              </div>
-            </div>
+        <Link
+          href={`/items/${verticalData.itemId}`}
+          className={styles.cardItemContainer}
+        >
+          <div className={styles.cardThumbCover}>
+            <Image
+              src={verticalData.thumbnail ?? defaultThumbnail}
+              sizes="33vw"
+              fill
+              alt="썸네일 이미지"
+              className={styles.cardThumbImage}
+            />
           </div>
-          {verticalData.rating && (
-            <div className={styles.cardBottom}>
-              <Rating ratingValue={verticalData.rating} />
-              {verticalData.comment && (
-                <p className={styles.singleLineComment}>
-                  {verticalData.comment}
-                </p>
-              )}
+          <div>
+            <div className={styles.cardTop}>
+              <div className={styles.cardLabel}>{verticalData.cardLabel}</div>
+              <div className={styles.cardTitle}>{verticalData.cardTitle}</div>
+              <div className={styles.cardText}>
+                <div className={styles.highlightText}>
+                  {verticalData.highlightText}
+                </div>
+                <div className={styles.crossOutText}>
+                  {verticalData.crossOutText}
+                </div>
+              </div>
             </div>
-          )}
-        </div>
+            {verticalData.rating && (
+              <div className={styles.cardBottom}>
+                <Rating ratingValue={verticalData.rating} />
+                {verticalData.comment && (
+                  <p className={styles.singleLineComment}>
+                    {verticalData.comment}
+                  </p>
+                )}
+              </div>
+            )}
+          </div>
+        </Link>
       </li>
     );
   } else {
     const horizontalData = data as HorizontalCardData;
     return (
       <li className={`${styles.cardItem} ${cardStyle}`}>
-        <div className={styles.cardThumbCover}>
-          <Image
-            src={horizontalData.thumbnail ?? defaultThumbnail}
-            sizes="33vw"
-            fill
-            alt="썸네일 이미지"
-            className={styles.cardThumbImage}
-          />
-        </div>
-        <div className={styles.cardReview}>
-          <div className={styles.commentTitle}>
-            {horizontalData.commentTitle}
+        <Link
+          href={`/reviews/${horizontalData.reviewId}`}
+          className={styles.cardItemContainer}
+        >
+          <div className={styles.cardThumbCover}>
+            <Image
+              src={horizontalData.thumbnail ?? defaultThumbnail}
+              sizes="33vw"
+              fill
+              alt="썸네일 이미지"
+              className={styles.cardThumbImage}
+            />
           </div>
-          <div className={styles.multiLiensComment}>
-            {horizontalData.comment}
+          <div className={styles.cardReview}>
+            <div className={styles.commentTitle}>
+              {horizontalData.commentTitle}
+            </div>
+            <div className={styles.multiLiensComment}>
+              {horizontalData.comment}
+            </div>
+            <div className={styles.ratingBlock}>
+              <Rating ratingValue={horizontalData.rating} />
+              <div className={styles.userName}>{horizontalData.userName}</div>
+            </div>
           </div>
-          <div className={styles.ratingBlock}>
-            <Rating ratingValue={horizontalData.rating} />
-            <div className={styles.userName}>{horizontalData.userName}</div>
-          </div>
-        </div>
+        </Link>
       </li>
     );
   }
