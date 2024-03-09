dd04749253f493e9215298cbca475515cb76d273
JeongSeonP <finchblue@naver.com>
2024-03-10 03:12:23 +0900

    :bug: Route handler config변경
    빌드시 캐시되는 과정에서의 에러 방지하기 위해 static이 아닌 dynamic으로 동작하도록 route.ts파일 업데이트

--- app/api/products/route.ts
+++ app/api/products/route.ts
@@ -1,5 +1,7 @@
 import { getProducts } from "@/db/cardMockData";
 
+export const dynamic = "force-dynamic";
+
 export async function GET() {
   const products = getProducts();
   return Response.json({ products });
--- app/api/reviews/route.ts
+++ app/api/reviews/route.ts
@@ -1,5 +1,7 @@
 import { getReviews } from "@/db/cardMockData";
 
+export const dynamic = "force-dynamic";
+
 export async function GET() {
   const reviews = getReviews();
   return Response.json({ reviews });
